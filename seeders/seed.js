d5bc2a9fdf96962b6c8db6976ef01cd016909da2
Jake Melville <jakemelville@ymail.com>
2021-03-31 17:55:47 -0400

    seeded db

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,6 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+let Workout = require("../models/workout.js") 
+// let db = require("./models");
 
 mongoose.connect("mongodb://localhost/fitness", {
   useNewUrlParser: true,
@@ -127,8 +128,8 @@ let workoutSeed = [
   }
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+Workout.deleteMany({})
+  .then(() => Workout.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
